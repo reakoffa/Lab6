6e18aabecfa1d70f9b87aaefaf69eee423924ed3
reakoffa <reakoffa@gmail.com>
2018-04-30 16:21:57 -0400

    comments added and challenge done

--- src/Lab6.java
+++ src/Lab6.java
@@ -6,21 +6,32 @@ public class Lab6 {
 	public static void main(String[] args) {
 		Scanner scnr = new Scanner(System.in);
 		String cont = " ";
+		// comments
 		do {
 			int side = Validator.getInt(scnr, "How many sides should each die have? ");
 			int num1 = RNG(side);
 			int num2 = RNG(side);
 			
+			//more comments
+			if (num1 + num2 == 2) {
+				System.out.println("Snake eyes");
+			}
+			if (num1 == 6 && num2 == 6) {
+				System.out.println("Boxcar racer");
+			}
+			if ((num1 + num2 == 7) || (num1 + num2 == 11)) {
+				System.out.println("Dont crap out on me now");
+			}
+			// last comment
 			cont = Validator.getString(scnr, "Would you like to continue? y/n");
 		} while (cont.equalsIgnoreCase("y"));
 		System.out.println("Goodbye!");
 
-		
-
 	}
 	
+	//method
 	public static int RNG(int Side) {
-		Random rng = new Random ();
+		Random rng = new Random();
 		int num = rng.nextInt(Side) + 1;
 		System.out.println(num);
 		return num;
